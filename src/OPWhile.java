159cb907b1a456c97911b99ab1a5ee7accf996c5
MTU2018 <zaykamoy83@mail.ru>
2018-05-30 19:03:26 +0300

    4

---
+++ src/OPWhile.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class OPWhile {
+    public static void main (String [] args){
+        int j = 0;
+        while (j < 2) {
+            System.out.println("Hello");
+            j++;}
+
+           Scanner scanner = new Scanner(System.in);
+         //   int a = scanner.nextInt();
+         //   int b = scanner.nextInt();
+         //   int c = 0;
+         //   if (a > b)
+         //       while (a - b >= 0) {
+        //            a = a - b;
+         //           System.out.println(
+         //                   ++c);}
+
+        int k = scanner.nextInt();
+        int w = 0;
+        while ((k+1)/2 == 0){
+
+        }
+        System.out.println(
+                ++w);}
+
+
+                }
+
+
+
