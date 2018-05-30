6753f5849b846c42cf34d2bafc947f3dc79c2eb6
MTU2018 <zaykamoy83@mail.ru>
2018-05-30 21:55:49 +0300

    6

---
+++ src/m.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class m {
+    public static void main (String[] args) {
+//        int [] array = {1,3,5,7,9};
+//        System.out.println(array [2]);
+//
+//        int [] array2 = new int[10];
+//        int j = 0;
+//        while (j< array2.length){
+//            array2 [j++] = j;
+//        }
+//        j=0;
+//        while (j<array2.length)
+//        System.out.println(array2 [j++]);
+
+       Scanner scanner = new Scanner(System.in);
+
+//       int n = scanner.nextInt();
+//        int k = scanner.nextInt();
+//       for (int j5 =0; j5<n; j5++)
+//           System.out.println(k);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        for (int j = a; j<=b; j++)
+            System.out.println(j);
+}}
+
